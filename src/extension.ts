fbd3173405739a4647c812df6cf35a561f7fa73a
Dinfeo <dinfeo@hotmail.com>
2024-10-22 18:52:57 +0800

    add select button

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,17 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
+    vscode.commands.registerCommand("run-button.select", () => {
+      vscode.commands.executeCommand("workbench.debug.action.focusRepl");
+      setTimeout(()=>{
+        vscode.commands.executeCommand("workbench.action.debug.selectandstart");
+      }, 500)
+    })
+  );
+  context.subscriptions.push(
     vscode.commands.registerCommand("run-button.debug", () => {
       vscode.commands.executeCommand("workbench.action.debug.start");
+      vscode.commands.executeCommand("workbench.debug.action.focusRepl");
     })
   );
   context.subscriptions.push(
